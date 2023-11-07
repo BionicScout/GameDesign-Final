6f5ce911ca9bb8a7b4a593cfe31268e8883a01b1
BionicScout <90854007+BionicScout@users.noreply.github.com>
2023-11-07 18:58:10 -0500

    Some Simple Grid Scripts (IN PROGRESS)

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+    MyGrid g = new MyGrid(5, 6, 2);
+
+    private void Start() {
+        //g.generateEmpty();
+    }
+
+
+}
