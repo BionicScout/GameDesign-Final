36e3f629afd37cd3bcb8d19303db79625e635800
BionicScout <90854007+BionicScout@users.noreply.github.com>
2023-11-10 14:59:53 -0500

    Set Up Combat Testing Scene

---
+++ Assets/Scripts/Combat_TESTING.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_TESTING : MonoBehaviour {
+    public FloorGrid grid;
+
+    private void Start() {
+        grid.generateEmpty();
+
+        GameObject playerObject = new GameObject();
+        playerObject.AddComponent<PlayerMovement>();
+        playerObject.GetComponent<PlayerMovement>().playerTile = grid.grid[0 , 0];
+        grid.grid[0 , 0].hasPlayer = true;
+        grid.grid[0 , 0].transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+
+}
