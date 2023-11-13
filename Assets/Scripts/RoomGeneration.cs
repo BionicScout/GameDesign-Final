0b3c72a1d87fd04a7c7ea564ce7b4ad834d27fe4
BionicScout <90854007+BionicScout@users.noreply.github.com>
2023-11-13 15:12:26 -0500

    Basic Procedural Room generation

--- Assets/Scripts/RoomGeneration.cs
+++ Assets/Scripts/RoomGeneration.cs
@@ -1,12 +1,17 @@
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RoomGeneration : MonoBehaviour {
-    int[,] roomLayout;
+    public int[,] roomLayout;
     public int gridSize;
+    public int howManyRooms;
 
-    struct RoomInfo {
+    int[,] baseDirections = { {-1, 0} , {0, -1}, {1, 0}, {0, 1} }; //[0 Up, 1 Left, 2 Down, 3 Right   ,   0 x, 1 y]
+
+    public struct RoomInfo {
         public int x, y;
         public bool[] directions; //0 Up, 1 Left, 2 Down, 3 Right. //True means room is in that direction
         public int doorsLeft;
@@ -21,7 +26,22 @@ public class RoomGeneration : MonoBehaviour {
             directions[2] = true;
             directions[3] = true;
 
-            doorsLeft = 0;
+            doorsLeft = 4;
+        }
+
+        public string print() {
+            string text = "";
+            text += "X: " + x + "\t";
+            text += "Y: " + y + "\n";
+
+            text += "Up: " + directions[0] + "\t";
+            text += "Left: " + directions[1] + "\t";
+            text += "Down: " + directions[2] + "\t";
+            text += "Right: " + directions[3] + "\n";
+
+            text += "Doors Left: " + doorsLeft + "\n";
+
+            return text;
         }
     }
 
@@ -30,96 +50,100 @@ public class RoomGeneration : MonoBehaviour {
         setToNegative1();
 
         int middle = Mathf.FloorToInt(gridSize / 2f);
-        roomLayout[middle, middle] = 0;
 
         //Generation
         List<RoomInfo> rooms = new List<RoomInfo>(); //Tuple represents a rooms x, y, and doorWaysLeft
 
         RoomInfo temp = new RoomInfo();
         temp.set(middle, middle);
+        roomLayout[middle , middle] = 0;
         rooms.Add(temp);
 
-        int roomsToGen = 25;
+        int roomsToGen = howManyRooms;
+
+        int fileText = 0;
+        print(fileText.ToString(), rooms);
+
+
 
         while(roomsToGen > 0) {
         //Get Room with doors
             Debug.Log("RoomsToGen: " + roomsToGen);
-            int i = Random.Range(0, rooms.Count);
+            int roomIndex = Random.Range(0, rooms.Count);
 
-            if(rooms[i].doorsLeft == 0)
+            //Debug.Log("Room Index: " + roomIndex);
+
+            if(rooms[roomIndex].doorsLeft == 0) {
+                Debug.Log("Max Doors!");
+                Debug.Log("END LOOP -----------------------------");
+                fileText++;
+                print(fileText.ToString(), rooms);
                 continue;
-
-        //Pick Direction of Room
-            int direction = Random.Range(0 , 3);
-            bool openDirection = rooms[i].directions[direction];
-
-            while(openDirection) {
-                direction = (direction+1) % 4;
-                openDirection = rooms[i].directions[direction];
             }
 
-        //Place Room
+
+            //Pick Direction of Room
+            int direction = Random.Range(0 , 4) % 4;
+            bool openDirection = rooms[roomIndex].directions[direction];
+
+            while(!openDirection) {
+                direction = (direction + 1) % 4;
+                openDirection = rooms[roomIndex].directions[direction];
+            }
+
+            Debug.Log("Direction: " + direction);
+            Debug.Log("Open: " + openDirection);
+
+
+            //Place Room
             temp = new RoomInfo();
 
-            if(direction == 0) {
-                temp.set(rooms[i].x, rooms[i].y + 1);
-            }
-            if (direction == 1) {
-                temp.set(rooms[i].x - 1, rooms[i].y);
-            }
-            if(direction == 2) {
-                temp.set(rooms[i].x, rooms[i].y - 1);
-            }
-            if(direction == 3) {
-                temp.set(rooms[i].x + 1, rooms[i].y);
-            }
+            temp.set(rooms[roomIndex].x + baseDirections[direction , 0] , rooms[roomIndex].y + baseDirections[direction , 1]);
+
             rooms.Add(temp);
-            rooms[i].directions[direction] = false;
-            roomLayout[temp.x, temp.y] = rooms.Count - 1;
+            //rooms[roomIndex].directions[direction] = false;
+            roomLayout[temp.x , temp.y] = rooms.Count - 1;
 
-        //Check The new room for near by rooms
-            if(roomLayout[temp.x , temp.y + 1] != -1) {
-                RoomInfo adjRoom = rooms[roomLayout[temp.x , temp.y + 1]];
-                adjRoom.doorsLeft--;
-                adjRoom.directions[0] = false;
+            //fileText++;
+            //print(fileText.ToString() , rooms);
 
-                temp.doorsLeft--;
-            }
-            if(roomLayout[temp.x - 1, temp.y] != -1) {
-                RoomInfo adjRoom = rooms[roomLayout[temp.x - 1 , temp.y]];
-                adjRoom.doorsLeft--;
-                adjRoom.directions[1] = false;
+            //Check The new room for near by rooms
+            for(int i = 0; i < baseDirections.GetLength(0); i++) {
+                //Debug.Log((temp.x + baseDirections[i , 0]) + ", " + (temp.y + baseDirections[i , 1]));
+                int roomId = roomLayout[temp.x + baseDirections[i , 0] , temp.y + baseDirections[i , 1]];
 
-                temp.doorsLeft--;
-            }
-            if(roomLayout[temp.x , temp.y - 1] != -1) {
-                RoomInfo adjRoom = rooms[roomLayout[temp.x , temp.y - 1]];
-                adjRoom.doorsLeft--;
-                adjRoom.directions[2] = false;
 
-                temp.doorsLeft--;
-            }
-            if(roomLayout[temp.x + 1, temp.y] != -1) {
-                RoomInfo adjRoom = rooms[roomLayout[temp.x + 1 , temp.y]];
-                adjRoom.doorsLeft--;
-                adjRoom.directions[3] = false;
+                if(roomId == -1)
+                    continue;
 
-                temp.doorsLeft--;
+                //Debug.Log("Did not continue");
+
+                if(rooms[roomId].doorsLeft > 0) {
+                    RoomInfo adjRoom = rooms[roomId];
+                    adjRoom.doorsLeft = adjRoom.doorsLeft - 1;                   ;
+                    adjRoom.directions[(i+2)%4] = false;
+                    rooms[roomId] = adjRoom;
+
+                    temp.doorsLeft--;
+                    temp.directions[i] = false;
+                }
             }
 
+            rooms[rooms.Count - 1] = temp;
 
 
 
-
-            roomLayout[temp.x , temp.y] = temp.doorsLeft;
+            //Debug.Log(temp.x + " " + temp.y);
+            //roomLayout[temp.x , temp.y] = rooms.Count - 1;
             roomsToGen--;
             Debug.Log("END LOOP -----------------------------");
+
+            fileText++;
+            print(fileText.ToString() , rooms);
         }
 
         
-        
-
-        print();
+       
     }
 
     public void setToNegative1() {
@@ -130,7 +154,7 @@ public class RoomGeneration : MonoBehaviour {
         }
     }
 
-    public void print() {
+    public void print(string fileName, List<RoomInfo> rooms) {
         string text = "";
 
         for(int x = 0; x < gridSize; x++) {
@@ -140,10 +164,28 @@ public class RoomGeneration : MonoBehaviour {
             text += "\n";
         }
 
-        StreamWriter writer1 = new StreamWriter("TestRoom.txt" , false);
+        text += "\n\n\n";
+
+        for(int x = 0; x < gridSize; x++) {
+            for(int y = 0; y < gridSize; y++) {
+                if(roomLayout[x, y] != -1)
+                    text += rooms[roomLayout[x , y]].doorsLeft.ToString() + "\t";
+                else
+                    text += "-1" + "\t";
+            }
+            text += "\n";
+        }
+
+        text += "\n\n\n";
+
+        for(int i = 0; i < rooms.Count; i++) {
+            text += "Room " + i + ":\n" + rooms[i].print() + "\n";
+        }
+
+        StreamWriter writer1 = new StreamWriter("TestRoom" + fileName + ".txt" , false);
         writer1.Write("");
         writer1.Close();
-        StreamWriter writer = new StreamWriter("TestRoom.txt" , true);
+        StreamWriter writer = new StreamWriter("TestRoom" + fileName + ".txt" , true);
         writer.Write(text);
         writer.Close();
     }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class Testing : MonoBehaviour {
@@ -16,6 +17,28 @@ public class Testing : MonoBehaviour {
         //grid.grid[0, 0].transform.GetChild(0).gameObject.SetActive(true);
 
         roomGen.generate();
+
+        
+
+        GameObject parent = new GameObject("Grid");
+        grid.grid = new FloorTile[grid.width , grid.height];
+
+        for(int w = 0; w < grid.width; w++) {
+            for(int h = 0; h < grid.height; h++) {
+                if(roomGen.roomLayout[w, h] != -1) {
+                    GameObject obj = Instantiate(grid.tile , new Vector3((w - 11) * grid.size, (h - 11) * grid.size, 0) , Quaternion.identity);
+                    obj.transform.SetParent(parent.transform);
+                    obj.transform.localScale *= grid.size;
+
+                    obj.AddComponent<FloorTile>();
+                    grid.grid[w , h] = obj.GetComponent<FloorTile>();
+                    grid.grid[w , h].floorCord = new int[2];
+                    grid.grid[w , h].floorCord[0] = w;
+                    grid.grid[w , h].floorCord[1] = h;
+                    grid.grid[w , h].floorGrid = grid;
+                }
+            }
+        }
 
 
     }
