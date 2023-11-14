030ec467920cdaaf8328b2dd2566eaf3439ef70b
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-11-14 14:39:49 -0500

    pause menu kinda works

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public string mainMenu = "MainMenu";
+    public GameObject pauseMenu;
+    public bool isPaused;
+    void Start()
+    {
+       pauseMenu.SetActive(false);
+    }
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+       {
+            PauseGame();
+       } 
+    }
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
     public void Menu()
     {
         SceneManager.LoadScene(mainMenu);
